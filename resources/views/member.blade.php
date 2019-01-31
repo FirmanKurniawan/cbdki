330fb30fc65811edd2a50d97731ccbc6b8b599e6
Firman Kurniawan <mrroz3r@gmail.com>
2019-01-31 17:59:37 +0700

    Font Awesome

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -1,31 +1,105 @@
 @extends('layouts.user')
 @section('content')
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
  <section id="subscribe">
       <div class="container wow fadeInUp">
         <div class="section-header">
-          <h2>Newsletter</h2>
-          <p>Rerum numquam illum recusandae quia mollitia consequatur.</p>
+          <h2>Cari Member</h2>
         </div>
 
-        <form method="POST" action="#">
           <div class="form-row justify-content-center">
             <div class="col-auto">
-              <input type="text" class="form-control" placeholder="Enter your Email">
+              <input type="text" class="form-control" placeholder="Masukkan No Identitas" name="nama" id="data">
             </div>
             <div class="col-auto">
-              <button type="submit">Subscribe</button>
+              <button class="btn btn-primary" id="submit" data-toggle="modal" data-target="#exampleModal">Cari</button>
             </div>
           </div>
-        </form>
 
       </div>
     </section>
 
+    {{-- AWAL JAVASCRIPT --}}
+    <script type="text/javascript">
+	   $(document).ready(function() {
+	    $('#submit').click(function() {
+	        $.ajaxSetup({
+	            headers: {
+	                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+	            }
+	        });
+	        var id = document.getElementById('data').value;
+	        $.ajax({
+	             type:"GET",
+	             url:"search2/" + id,
+	             success : function(results) {
+	              if (results.no_identitas) {
+	                  document.getElementById('nama').innerHTML =
+	                    '<label>Nama : </label>' +
+	                    '<input class="form-control" type="text" readonly style="background-color:#fff;" value="'+results.nama+'">';
+	                  document.getElementById('alamat').innerHTML =
+	                    '<label>Alamat : </label>' +
+	                    '<input class="form-control" type="text" readonly style="background-color:#fff;" value="'+results.alamat+'">';
+	                  document.getElementById('no_identitas').innerHTML =
+	                    '<label>No Identitas : </label>' +
+	                    '<input class="form-control" type="text" readonly style="background-color:#fff;" value="'+results.no_identitas+'">';
+	                  $('#no_identitas').val(results.no_identitas)
+	                  document.getElementById('gambar').innerHTML = 
+	                    '<img src="images/'+results.foto+' "class="bulat2" style="margin-left:30px;"/>';
+	                  }else{
+	                    document.getElementById('no_identitas1').innerHTML =
+	                    '<center><h1><i class="fas fa-times-circle fa-6x" style="color:red;margin-bottom:50px;margin-left:30px;"></i></h1></center>'+
+	                    '<h2 style="margin-left:30px;">IDENTITAS TIDAK TERSEDIA</h2>';
+	                  }
+	             }
+	        }); 
+	    });
+	}); 
+	</script>
+    {{-- AKHIR JAVASCRIPT --}}
+
+    {{-- AWAL MODAL --}}
+    <!-- Modal -->
+	<div class="modal fade" id="exampleModal">
+	  <div>
+	    <div class="modal-content" style="width: 500px;height: auto;margin-left: auto;margin-right: auto;margin-top: 7%;">
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel">Cari Anggota</h5>
+	        <form action="{{url('/member')}}">
+	        <button type="submit" class="close" aria-label="Close">
+	          <span aria-hidden="true">&times;</span>
+	        </button>
+	        </form>
+	      </div>
+	      <div class="modal-body">
+	      <div class="row">
+	        <div class="float-left">
+	          <br>
+	         <div id="gambar" class="img-fluid">
+	         </div>
+	         </div>
+	         <div class="float-right" style="margin-left: 30px;">  
+	          <div id="nama"></div>
+	      <br>
+	      <div id="alamat"></div>
+	      <br>
+	      <div id="no_identitas"></div>
+	      <div id="no_identitas1"></div>
+	          <!-- {{-- <img src="{{url('images/'.$foto)}}"> --}} -->
+
+			</div>
+		</div>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+    {{-- AKHIR MODAL --}}
+
 <section id="schedule" class="section-with-bg">
       <div class="container wow fadeInUp">
         <div class="section-header">
-          <h2>Event Schedule</h2>
-          <p>Here is our event schedule</p>
+          <h2>Kordinasi Wilayah</h2>
         </div>
 
         <ul class="nav nav-tabs" role="tablist">
