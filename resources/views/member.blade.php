d410336f9a21290ff0558c4c56b5d544c818ff95
Firman Kurniawan <mrroz3r@gmail.com>
2019-02-01 16:54:39 +0700

    Search

--- resources/views/member.blade.php
+++ resources/views/member.blade.php
@@ -4,7 +4,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.1/bootstrap3-typeahead.min.js"></script>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 <style type="text/css">
-	.bulat{
+  .bulat{
 border-radius:100em;
 opacity:1;
 width:200px;
@@ -22,7 +22,7 @@ height:200px;
               <input type="text" class="form-control" placeholder="Masukkan No Identitas" name="nama" id="data">
             </div>
             <div class="col-auto">
-              <button class="btn btn-primary" id="submit" data-toggle="modal" data-target="#exampleModal">Cari</button>
+              <button class="btn btn-primary" id="submit">Cari</button>
             </div>
           </div>
       </div>
@@ -30,7 +30,6 @@ height:200px;
 
     {{-- AWAL JAVASCRIPT --}}
     <script type="text/javascript">
-<<<<<<< HEAD
      $(document).ready(function() {
       $('#submit').click(function() {
           $.ajaxSetup({
@@ -44,164 +43,72 @@ height:200px;
                url:"search2/" + id,
                success : function(results) {
                 if (results.no_identitas) {
-                    document.getElementById('nama').innerHTML =
+                  document.getElementById('nama').innerHTML =
                       '<label>Nama : </label>' +
                       '<input class="form-control" type="text" readonly style="background-color:#fff;" value="'+results.nama+'">';
-                    document.getElementById('alamat').innerHTML =
-                      '<label>Alamat : </label>' +
+                  document.getElementById('alamat').innerHTML =
+                      '<label>alamat : </label>' +
                       '<input class="form-control" type="text" readonly style="background-color:#fff;" value="'+results.alamat+'">';
-                    document.getElementById('no_identitas').innerHTML =
+                  document.getElementById('no_identitas').innerHTML =
                       '<label>No Identitas : </label>' +
                       '<input class="form-control" type="text" readonly style="background-color:#fff;" value="'+results.no_identitas+'">';
-                    $('#no_identitas').val(results.no_identitas)
-                    document.getElementById('gambar').innerHTML =
-                      '<img src="images/'+results.foto+' "class="bulat2" style="margin-left:auto;width:30%;"/>';
+                  document.getElementById('gambar').innerHTML =
+                      '<img src="images/'+results.foto+' "class="bulat2" style="width:20%;"/>';
+                    $(document).ready(function(){
+                     
+                              $('#modal-kotak , #bg').fadeIn("slow");
+                               
+                          $('#tombol-tutup').click(function(){
+                            $('#modal-kotak , #bg').fadeOut("slow");
+                          });
+                        });
                     }else{
-                      document.getElementById('no_identitas1').innerHTML =
-                      '<center><h1><i class="fas fa-times-circle fa-3x"></i></h1></center>'+
-                      '<h2>IDENTITAS TIDAK TERSEDIA</h2>';
+                      document.getElementById('nama23').innerHTML =
+                      '<label>Data Tidak Ditemukan</label>';
+                        $(document).ready(function(){
+                              $('#modal-kotak2 , #bg').fadeIn("slow");
+                          $('#tombol-tutup2').click(function(){
+                            $('#modal-kotak2 , #bg').fadeOut("slow");
+                          });
+                        });
                     }
                }
           }); 
       });
   }); 
   </script>
-=======
-	   $(document).ready(function() {
-	    $('#submit').click(function() {
-	        $.ajaxSetup({
-	            headers: {
-	                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-	            }
-	        });
-	        var id = document.getElementById('data').value;
-	        $.ajax({
-	             type:"GET",
-	             url:"search2/" + id,
-	             success : function(results) {
-	              if (results.no_identitas) {
-	                  document.getElementById('nama').innerHTML =
-	                    '<label>Nama : </label>' +
-	                    '<input class="form-control" type="text" readonly style="background-color:#fff;" value="'+results.nama+'">';
-	                  document.getElementById('alamat').innerHTML =
-	                    '<label>Alamat : </label>' +
-	                    '<input class="form-control" type="text" readonly style="background-color:#fff;" value="'+results.alamat+'">';
-	                  document.getElementById('no_identitas').innerHTML =
-	                    '<label>No Identitas : </label>' +
-	                    '<input class="form-control" type="text" readonly style="background-color:#fff;" value="'+results.no_identitas+'">';
-	                  $('#no_identitas').val(results.no_identitas)
-	                  document.getElementById('gambar').innerHTML =
-	                    '<img src="images/'+results.foto+' "class="bulat2" style="margin-left:auto;width:30%;"/>';
-	                  }else{
-	                    document.getElementById('no_identitas1').innerHTML =
-	                    '<center><h1><i class="fas fa-times-circle fa-3x"></i></h1></center>'+
-	                    '<h2>IDENTITAS TIDAK TERSEDIA</h2>';
-	                  }
-	             }
-	        }); 
-	    });
-	}); 
-	</script>
->>>>>>> 6182c3459be5a6af90a3100677002258f6dc571f
     {{-- AKHIR JAVASCRIPT --}}
-
     {{-- AWAL MODAL --}}
-    <!-- Modal -->
-<<<<<<< HEAD
-  <div class="modal fade" id="exampleModal">
+  <div class="container" id="modal-kotak">
     <div>
-      <div class="modal-content" style="width: 100%;height: 100%;margin-left: auto;margin-right: auto;">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Cari Anggota</h5>
-          <form action="{{url('/member')}}">
-          <button type="submit" class="close" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-          </form>
-        </div>
-        <div class="modal-body">
-        <div class="row">
-          <div class="float-left">
-            <br>
-           <div id="gambar" class="img-fluid"></div>
-           </div>
-           <div class="float-right" style="margin-left: 30px;">
-            {{--  --}}
-            <div class="row" id="nama">
-=======
-	<div class="modal fade" id="exampleModal">
-	  <div>
-	    <div class="modal-content" style="width: 100%;height: 100%;margin-left: auto;margin-right: auto;">
-	      <div class="modal-header">
-	        <h5 class="modal-title" id="exampleModalLabel">Cari Anggota</h5>
-	        <form action="{{url('/member')}}">
-	        <button type="submit" class="close" aria-label="Close">
-	          <span aria-hidden="true">&times;</span>
-	        </button>
-	        </form>
-	      </div>
-	      <div class="modal-body">
-	      <div class="row">
-	        <div class="float-left">
-	          <br>
-	         <div id="gambar" class="img-fluid"></div>
-	         </div>
-	         <div class="float-right" style="margin-left: 30px;">
-            {{--  --}}
-	          <div class="row" id="nama">
->>>>>>> 6182c3459be5a6af90a3100677002258f6dc571f
-            <div class="col">
-            </div>
-            <div class="col">
-            </div>
+     
+      <div class="modal-body">
+          <center>
+          <div id="gambar"></div>
+          </center>
+          <center>
+          <div class="col-md-6">
+          <div id="nama"></div>
+          <div id="alamat"></div>
+          <div id="no_identitas"></div>
+          <div id="bawah">
+            <button id="tombol-tutup" class="btn btn-primary">Tutup</button>
           </div>
-          {{--  --}}
-<<<<<<< HEAD
-        <br>
-        {{--  --}}
-=======
-	      <br>
-	      {{--  --}}
->>>>>>> 6182c3459be5a6af90a3100677002258f6dc571f
-            <div class="row" id="alamat">
-            <div class="col">
-            </div>
-            <div class="col">
-            </div>
           </div>
-          {{--  --}}
-<<<<<<< HEAD
-        <br>
-        {{--  --}}
-=======
-	      <br>
-	      {{--  --}}
->>>>>>> 6182c3459be5a6af90a3100677002258f6dc571f
-            <div class="row" id="no_identitas">
-            <div class="col">
-            </div>
-            <div class="col">
-            </div>
-          </div>
-          {{--  --}}
-	      <div id="no_identitas1" style="margin-left: auto;margin-right: auto;"></div>
-	          <!-- {{-- <img src="{{url('images/'.$foto)}}"> --}} -->
-
-<<<<<<< HEAD
-      </div>
-    </div>
-        </div>
+          </center>
       </div>
     </div>
   </div>
-=======
-			</div>
-		</div>
-	      </div>
-	    </div>
-	  </div>
-	</div>
->>>>>>> 6182c3459be5a6af90a3100677002258f6dc571f
+  <div id="bg"></div>
+  <div id="modal-kotak2">
+    <div id="atas">
+      <center>
+      <div hidden="true" id="nama23"></div>
+      <button id="tombol-tutup2" class="btn btn-primary" aria-label="Close">Data Tidak Ditemukan</button>
+      </div>
+      </center>
+    </div>
+  </div>
     {{-- AKHIR MODAL --}}
 
 <section id="schedule" class="section-with-bg">
@@ -210,8 +117,8 @@ height:200px;
           <h2>Kordinasi Wilayah</h2>
         </div>
         <?php
-        	$i = 1;
-        	$kiwil = App\Korwil::all();
+          $i = 1;
+          $kiwil = App\Korwil::all();
         ?>
         @foreach($kiwil as $k)
         <div style="margin-top: 2%;">
@@ -255,6 +162,65 @@ height:200px;
         </div>
         @endforeach
       </div>
-
+<style type="text/css">
+  body{
+  background: #ecf0f1;
+  font-family: sans-serif;
+  font-size: 11pt;
+}
+#modal-kotak{
+  position:sticky;
+  z-index:1002;
+  display: none;
+  background: white;  
+  border-radius: 10px;
+}
+#modal-kotak2{
+  height: auto;
+  width: auto;
+  position:relative;
+  z-index:1002;
+  display: none;
+  background: white;  
+}
+#atas{
+  font-size: 15pt;
+  padding: 20px;  
+  height: 80%;
+}
+#bawah{
+  background: #fff;
+}
+ 
+#tombol-tutup{
+  margin-top: 10px;
+}
+#tombol-tutup2{
+  margin-top: 10px;
+  margin-left: 1%;
+}
+#tombol-tutup,#tombol{
+  height: 30px;
+  width: 100px;
+  color: #fff;
+  border: 0px;
+}
+#bg{
+  opacity:.80;
+  position: absolute;
+  display: none;
+  top: 0%;
+  left: 0%;
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+  z-index:1001;
+  opacity: 0.8;
+}
+#tombol{
+  background: #e74c3c;        
+}
+}
+</style>
     </section>
     @endsection
